4437e10962a392690109f17f92c22add6c1f80ca
Vadim Prokoshin <vpro85@gmail.com>
2022-12-05 21:59:39 +0200

    Add axios config

---
+++ src/axiosConfig.js
@@ -0,0 +1,12 @@
+import Axios from "axios";
+
+const instance = Axios.create({
+    headers: {
+        "Access-Control-Allow-Origin": "http://localhost:3000"
+    },
+    withCredentials: false,
+    responseType: "json",
+    maxRedirects: 10,
+});
+
+export default instance;
