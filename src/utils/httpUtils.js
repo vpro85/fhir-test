cee733dbf956c869797bcf75f67efe559e48a4be
Vadim Prokoshin <vpro85@gmail.com>
2022-12-05 22:37:28 +0200

    Add HttpUtils (with http headers)

---
+++ src/utils/httpUtils.js
@@ -0,0 +1,10 @@
+const httpHeaders = {
+    headers: {
+        "Access-Control-Allow-Origin": "http://localhost:3000",
+    },
+};
+const HttpUtil = {
+    authHttpHeaders: httpHeaders,
+}
+
+export default HttpUtil;
