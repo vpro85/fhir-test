7ada103e75e326afa1f7d54a819e8c9ca972a813
Vadim Prokoshin <vpro85@gmail.com>
2022-12-06 01:31:36 +0200

    implement fetch and set patient ids

--- src/views/App.js
+++ src/views/App.js
@@ -5,18 +5,37 @@ import {projectApi} from "../api/projectApi";
 
 export default function App() {
     const [appointments, setAppointments] = useState([])
+    const [patientIds, setPatientIds] = useState([])
 
     async function fetchAppointmentsData() {
         const {entry} = await projectApi.getAppointments();
         setAppointments(entry)
+    }
 
-    };
     useEffect(() => {
         fetchAppointmentsData();
     }, [])
 
     useEffect(() => {
-        console.log("appointments", appointments)
+        patientIds.length > 0 && console.log("patientIds:", patientIds)
+    }, [patientIds])
+
+    useEffect(() => {
+        const patients = []
+        if (appointments.length > 0) {
+            appointments.forEach((data) => {
+                const participant = data.resource.participant ??= null;
+                participant && participant.forEach((data) => {
+                    const reference = data?.actor?.reference;
+                    if (reference && reference.includes("Patient")) {
+                        patients.push(reference.slice(8))
+                    }
+                })
+            })
+        }
+        const patientsList = []
+        new Set(patients).forEach(p => patientsList.push(p))
+        setPatientIds(patientsList)
     }, [appointments])
 
     return (
